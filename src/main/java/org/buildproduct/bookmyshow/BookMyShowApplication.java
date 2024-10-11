5d4b2e47abbdd28b13abb21ce0cb06a9079f1983
DevMhrn <debashismaharana7854@gmail.com>
2024-10-11 18:34:53 +0530

    Added the folders

---
+++ src/main/java/org/buildproduct/bookmyshow/BookMyShowApplication.java
@@ -0,0 +1,13 @@
+package org.buildproduct.bookmyshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyShowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMyShowApplication.class, args);
+    }
+
+}
